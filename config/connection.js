899e2c3d0ef4aa35edafb4507321c57aa9552f29
woffordlm <lukewofford@gmail.com>
2022-02-25 12:27:56 -0500

    changed connections file

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize("cocktails", "root", "password", {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
