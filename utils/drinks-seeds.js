aef1d41bdadd14ae91ae8e09c65b8d111b7f1a49
woffordlm <lukewofford@gmail.com>
2022-02-24 21:11:29 -0500

    getting ready to pull

--- utils/drinks-seeds.js
+++ utils/drinks-seeds.js
@@ -2,9 +2,10 @@ const { Drink } = require('../models');
 
 const drinkdata = [
   {
+    name: "apple jack",
     image_url: 'placeholder',
     category_id: 1,
-    ingredients: ["apple juice", "vodka"],
+    ingredients: "apple juice and vodka",
     glass_type: "coupe",
     instructions: "mix and drink"
   }
