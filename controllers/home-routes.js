3838e1f76a1ccf0b563b1bfb492779fcc7e8bbc3
mcnairjm19 <mcnairjm1993@gmail.com>
2022-02-27 16:32:59 -0500

    handlebars working

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,20 +1,10 @@
 const router = require('express').Router();
-<<<<<<< HEAD
-
-router.get('/', (req, res) => {
-  res.render('homepage');
-});
-
-router.get('/favorites', (req, res) => {
-  res.render('favorites');
-});
-=======
 const sequelize = require('../config/connection');
 const { Category, User, Star, Comment, Drink } = require('../models');
 
-router.get('/', (req, res) => {
+router.get('/drinks', (req, res) => {
     Drink.findAll({
-        attributes: [
+        /*attributes: [
           "id",
           "image_url",
           "name",
@@ -42,11 +32,13 @@ router.get('/', (req, res) => {
             model: Category,
             attributes: ["id", "name"]
           },
-        ],
+        ],*/
       })
       .then(dbDrinkData => {
+        console.log('!!!!!!!!!!!!!!!!!!!!!!!',dbDrinkData);
         const drinks = dbDrinkData.map(drink => drink.get({ plain: true }));
-        res.render('homepage', {
+  
+        res.render('drinks', {
           drinks,
           loggedIn: req.session.loggedIn
         });
@@ -70,7 +62,7 @@ router.get('/', (req, res) => {
     Drink.findOne({
       where: {
         id: req.params.id
-      },
+      }/*,
       attributes: [
         "id",
         "image_url",
@@ -99,7 +91,7 @@ router.get('/', (req, res) => {
           model: User,
           attributes: ['username']
         }
-      ]
+      ]*/
     })
       .then(dbDrinkData => {
         if (!dbDrinkData) {
@@ -139,7 +131,53 @@ router.get('/stars', (req, res) => {
         res.status(500).json(err);
       });
 })
->>>>>>> ec3037db1e7cb3012182bd8f45133d0426b0fcc9
+
+router.get('/drinks', (req, res) => {
+  Drink.findAll({
+      attributes: [
+        "id",
+        "image_url",
+        "name",
+        "category_id",
+        "ingredients",
+        "glass_type",
+        "instructions",
+        [
+          sequelize.literal(
+            "(SELECT COUNT(*) FROM star WHERE drink.id = star.drink_id)"
+          ),
+          "star_count",
+        ],
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ["id", "comment_text", "drink_id", "user_id", "created_at"],
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
+        },
+        {
+          model: Category,
+          attributes: ["id", "name"]
+        },
+      ],
+    })
+    .then(dbDrinkData => {
+      console.log('!!!!!!!!!!!!!!!!!!!!!!!',dbDrinkData);
+      const drinks = dbDrinkData.map(drink => drink.get({ plain: true }));
+
+      res.render('drinks', {
+        /*drinks,
+        loggedIn: req.session.loggedIn*/
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 
 module.exports = router;
---
+++ public/js/script.js
@@ -0,0 +1,3 @@
+fetch('')
+  .then(response => response.json())
+  .then(data => console.log(data));
