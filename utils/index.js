ccc1c16c38c7d0994ad2ecbb6b4960887e78e713
woffordlm <lukewofford@gmail.com>
2022-03-02 16:40:49 -0500

    checking server

--- utils/index.js
+++ utils/index.js
@@ -6,7 +6,7 @@ const seedCategories = require ('./category-seeds');
 const sequelize = require('../config/connection');
 
 const seedAll = async () => {
-    await sequelize.sync({ force: true });
+    await sequelize.sync({ force: false });
      //console.log('--------------');
 
     await seedCategories();
