afa3d8bcdf2f1c0200a0a6edf66d6b029c7584aa
woffordlm <lukewofford@gmail.com>
2022-03-05 08:51:33 -0500

    erased error on main view

--- public/js/login.js
+++ public/js/login.js
@@ -48,7 +48,6 @@ async function signupFormHandler(event) {
     }
   }
   
-  
   document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
   
   document.querySelector('#signup-form').addEventListener('submit', signupFormHandler);
