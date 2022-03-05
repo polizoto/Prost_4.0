3ae0885bee65f23102a7b563e716f2c4e4ecc3ab
polizoto <polizoto@hotmail.com>
2022-03-05 16:15:56 -0500

    removed path directory from express static declaration

--- server.js
+++ server.js
@@ -61,10 +61,10 @@ const PORT = process.env.PORT || 3001;
 
 app.use(session(sess));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.urlencoded({ extended: false })); // was true
+app.use(express.static('public')); // was (path.join(__dirname, 'public')
 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
